fbd392022e6f6c2adcc3437c06d863f4a622205b
HEY-SUBRAT <114894383+HEY-SUBRAT@users.noreply.github.com>
2025-07-18 12:04:27 +0530

    Model_Trainer

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from src.mlproject.components.data_ingestion import DataIngestionConfig
 
 from src.mlproject.components.data_transformation import DataTransformationConfig
 from src.mlproject.components.data_transformation import DataTransformation
-
+from src.mlproject.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 import sys
 
@@ -16,15 +16,19 @@ if __name__ == "__main__":
 
 
     try:
-        
-        data_ingestion_config = DataIngestionConfig()
+
+        # data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
         train_data_path,test_data_path = data_ingestion.initiate_data_ingestion()
 
 
         # data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        train_arr, test_arr,_ = data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+        ##model training
+        model_trainer =  ModelTrainer()
+        print( model_trainer.initiate_model_trainer(train_arr,test_arr))
 
        
 
